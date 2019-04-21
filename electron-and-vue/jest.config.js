bf0f97671630595cfd5248dbe87719647a167057
yuyasvx <yuyasvx@gmail.com>
2019-04-21 16:44:18 +0900

    fix config

--- electron-and-vue/jest.config.js
+++ electron-and-vue/jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: ['**/tests/unit/**/*.(spec|test).(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
+  testMatch: ['<rootDir>/tests/unit/**/*.(spec|test).(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
   collectCoverage: true,
   collectCoverageFrom: [
--- electron-and-vue/tests/unit/renderer/example.spec.ts
+++ electron-and-vue/tests/unit/renderer/example.spec.ts
@@ -1,7 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import HelloWorld from '@/renderer/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
+  it('tests mount', () => {
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
